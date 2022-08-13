96fa997a7a1c9779c2a90452e9199a51da21492c
Jesus Medrano <100323659+JesusMH98@users.noreply.github.com>
2022-08-13 15:18:38 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -305,12 +305,26 @@ output.pop;
 var output = [];
 var count = 1;
 
-function  fuzzBuzz() {
+function fizzBuzz() {
 
-    output.push(count);
+    if(count % 3 === 0 && count % 5 === 0)
+    {
+        output.push("FizzBuzz");
+    }
+    else if(count % 3 === 0)
+    {
+        output.push("Fizz");
+    }
+    else if(count % 5 === 0)
+    {
+        output.push("Buzz");
+    }
+    else
+    {
+        output.push(count);
+    }
 
     count++;
-    
     console.log(output);
     
 }
